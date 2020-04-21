d4881d96d3defb7265d345c51dc3d02fc52fadbb
unknown <eder.cornejo.diaz@ucsp.edu.pe>
2020-04-21 14:14:08 -0500

    validacion por token

--- app.js
+++ app.js
@@ -1,27 +1,46 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 // Inicializar variables
 var app = express();
 
+// Body-parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+    // parse application/json
+app.use(bodyParser.json())
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 // Conexión a la base de datos
-mongoose.connection.openUri('mongodb://localhost:27017/ondb', (err, res) => {
-    if (err) throw err;
+mongoose.connect('mongodb://localhost:27017/ondb', {
+        useCreateIndex: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }, (err, res) => {
 
-    console.log('Base de datos: \x1b[41m%s\x1b[0m',
-        'online ');
-})
+        if (err) throw err;
 
+        console.log('Base de datos: \x1b[41m%s\x1b[0m',
+            'online ');
+    })
+    // mongoose.connection.openUri('mongodb://localhost:27017/ondb', (err, res) => {
 
-// Escuchar peticiones
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Petición realizada correctamente'
-    });
-});
+//     if (err) throw err;
+
+//     console.log('Base de datos: \x1b[41m%s\x1b[0m',
+//         'online ');
+// });
+
+// Rutas
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
